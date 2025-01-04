87a721d09e3487f9c1af3f29cc1a67c9173b91c6
Chrawnus <asplund.jonathan@gmail.com>
2025-01-04 21:45:41 +0200

    add rotate and update function for Player class

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	dt = 0
 
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		
+			
+		player.update(dt)
+
 		screen.fill((0,0,0))
 		player.draw(screen)
 		pygame.display.flip()
